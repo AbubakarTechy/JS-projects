a493a2ae09134a033fb91ffa217ee79ccab071c3
Abubakar Rasheed <131538521+AbubakarTechy@users.noreply.github.com>
2024-08-01 13:54:15 +0500

    Add files via upload

---
+++ Heart/index.js
@@ -0,0 +1,22 @@
+const bodyElement= document.querySelector("body");
+
+bodyElement.addEventListener("mousemove",(event)=>{
+        const xPos=event.offsetX;
+        const yPos=event.offsetY;
+        const spanEl=document.createElement("span");
+        spanEl.style.left=xPos + "px";
+        spanEl.style.top=yPos + "px";
+
+        const size=Math.random()*100;
+        spanEl.style.width=size +"px";
+        spanEl.style.height= size+"px";
+
+        bodyElement.appendChild(spanEl);
+
+        setTimeout(() => {
+                spanEl.remove();
+            }, 700);
+
+});
+
+
