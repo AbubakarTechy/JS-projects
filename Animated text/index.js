1a84b1d706025a18fd45d08fde12b3043eec53e9
Abubakar Rasheed <131538521+AbubakarTechy@users.noreply.github.com>
2024-08-05 11:27:24 +0500

    Add files via upload

---
+++ Animated text/index.js
@@ -0,0 +1,13 @@
+const BodyHun=document.getElementById("animated-text");
+const text=["I am web Dev" , "I am Freelancer", "I am Tutor", "I am Youtuber" ];
+let index=0;
+
+function textChange(){
+    BodyHun.textContent=text[index];
+    index++;
+    if(index >= text.length){
+        index=0;
+    }
+    setTimeout(textChange,1000);
+}
+textChange();
---
+++ Mini Calendar/index.js
@@ -0,0 +1,17 @@
+const monthName=document.getElementById("month-name");
+const dayName= document.getElementById("day-name")
+const dayNumber= document.getElementById("day-number");
+const YearN= document.getElementById("year");
+
+const date= new Date();
+
+const month=date.getMonth()
+monthName.innerText=date.toLocaleString("eng", {
+    month:"long"
+})
+dayName.innerText=date.toLocaleString("eng",{
+    weekday:"long"
+})
+dayNumber.innerText=date.getDate();
+
+YearN.innerText=date.getFullYear(); 
